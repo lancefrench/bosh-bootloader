c92a196533fbdc7d0da31fb4459cce568cce12f6
Nick Mahoney <nmahoney@pivotal.io>
2017-10-04 16:55:10 -0700

    WIP: add acceptance test
    [#151612121]
    
    Signed-off-by: Rowan Jacobs <rojacobs@pivotal.io>

--- acceptance-tests/bbl/up_test.go
+++ acceptance-tests/bbl/up_test.go
@@ -2,7 +2,9 @@ package acceptance_test
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"time"
 
 	acceptance "github.com/cloudfoundry/bosh-bootloader/acceptance-tests"
@@ -13,13 +15,14 @@ import (
 	"github.com/onsi/gomega/gexec"
 )
 
-var _ = Describe("up", func() {
+var _ = FDescribe("up", func() {
 	var (
 		bbl             actors.BBL
 		boshcli         actors.BOSHCLI
 		directorAddress string
 		caCertPath      string
 		sshSession      *gexec.Session
+		stateDir        string
 
 		boshDirectorChecker actors.BOSHDirectorChecker
 	)
@@ -28,7 +31,9 @@ var _ = Describe("up", func() {
 		configuration, err := acceptance.LoadConfig()
 		Expect(err).NotTo(HaveOccurred())
 
-		bbl = actors.NewBBL(configuration.StateFileDir, pathToBBL, configuration, "up-env")
+		stateDir = configuration.StateFileDir
+
+		bbl = actors.NewBBL(stateDir, pathToBBL, configuration, "up-env")
 		boshcli = actors.NewBOSHCLI()
 
 		boshDirectorChecker = actors.NewBOSHDirectorChecker(configuration)
@@ -52,6 +57,71 @@ var _ = Describe("up", func() {
 		acceptance.SkipUnless("bbl-up")
 		session := bbl.Up("--name", bbl.PredefinedEnvID())
 		Eventually(session, 40*time.Minute).Should(gexec.Exit(0))
+
+		By("verifying that artifacts are created in state dir", func() {
+			_, err := os.Stat(filepath.Join(stateDir, "bbl-state.json"))
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "cloudconfig", "cloud-config.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "cloudconfig", "ops.yml"))
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "aws-bosh-director-encrypt-disk-ops.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "aws-bosh-director-ephemeral-ip-ops.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "aws-external-ip-not-recommended.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "azure-external-ip-not-recommended.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "azure-ssh-static-ip.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "bosh.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "cpi.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "credhub.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "gcp-bosh-director-ephemeral-ip-ops.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "gcp-external-ip-not-recommended.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "iam-instance-profile.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "jumpbox-user.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "uaa.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "bosh-deployment", "user-ops-file.yml"))
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "jumpbox-deployment", "cpi.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "jumpbox-deployment", "jumpbox.yml"))
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = os.Stat(filepath.Join(stateDir, "bosh", "previous-user-ops-file.yml"))
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = os.Stat(filepath.Join(stateDir, "vars", "terraform-credentials.json"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "vars", "bosh-deployment-state.json"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "vars", "bosh-deployment-variables.json"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "vars", "jumpbox-deployment-state.json"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "vars", "jumpbox-deployment-variables.json"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "vars", "user-ops-file.yml"))
+			Expect(err).NotTo(HaveOccurred())
+			_, err = os.Stat(filepath.Join(stateDir, "vars", "terraform.tfstate"))
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = os.Stat(filepath.Join(stateDir, "terraform", "template.tf"))
+			Expect(err).NotTo(HaveOccurred())
+		})
 
 		By("creating an ssh tunnel to the director in print-env", func() {
 			sshSession = bbl.StartSSHTunnel()
@@ -109,6 +179,15 @@ var _ = Describe("up", func() {
 		By("destroying the director and the jumpbox", func() {
 			session := bbl.Down()
 			Eventually(session, 10*time.Minute).Should(gexec.Exit(0))
+		})
+
+		By("verifying that artifacts are removed from state dir", func() {
+			f, err := os.Open(stateDir)
+			Expect(err).NotTo(HaveOccurred())
+
+			filenames, err := f.Readdirnames(0)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(filenames).To(BeEmpty())
 		})
 	})
 })
